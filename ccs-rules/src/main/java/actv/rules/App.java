e62e9a871303b424a482292f0339e842702273b2
Thomas Yang <tlyyang@gmail.com>
2015-02-12 09:57:31 -0800

    Added a second project (the software rules) as a dependency.

---
+++ ccs-rules/src/main/java/actv/rules/App.java
@@ -0,0 +1,13 @@
+package actv.rules;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
