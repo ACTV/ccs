039362adc83f64ef012ec3e512784182746a2b1c
andphan <amannamedphan@gmail.com>
2015-03-19 11:26:39 -0700

    interface this later

--- ccs-model/src/main/java/actv/ccs/model/ui/Collection.java
+++ ccs-model/src/main/java/actv/ccs/model/ui/Collection.java
@@ -3,6 +3,6 @@ package a4;
 import java.util.Vector;
 
 public interface Collection {
-	public void add(GameObject obj);
-	public Vector<GameObject> getObjects();
+//	public void add(GameObject obj);
+//	public Vector<GameObject> getObjects();
 }
