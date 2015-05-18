66c6ee857ebac4fec1cd97b25afc2738c6cc9ee5
Thomas Yang <tlyyang@gmail.com>
2015-05-18 01:48:04 -0700

    Update runner

--- ccs/src/main/java/actv/ccs/sageTest/AI/AIRunner.java
+++ ccs/src/main/java/actv/ccs/sageTest/AI/AIRunner.java
@@ -3,6 +3,11 @@ package actv.ccs.sageTest.AI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Runnable class to start the AI of the fish
+ * 
+ * @see AIController
+ */
 public class AIRunner implements Runnable {
 	private AIController aic;
 	private static final Logger logger = LoggerFactory.getLogger(AIRunner.class);
@@ -13,18 +18,6 @@ public class AIRunner implements Runnable {
 
 	public void run() {
 		logger.debug("Executing AI!");
-		long frameStartTime;
-		float elapMilSecs;
-		long lastUpdateTime=0;
-		
-		aic.setupAI();
-		while (true){
-			frameStartTime = System.nanoTime();
-			elapMilSecs = (frameStartTime-lastUpdateTime)/(1000000.0f);
-			if (elapMilSecs >= 50.0f){
-				aic.update();
-			}
-		}
+		aic.startAI();
 	}
-
 }
